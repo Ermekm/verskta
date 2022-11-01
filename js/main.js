1f1312a655ed29ed70b40574dab3315a45b44cfe
ermekm <ermek.mrzv@gmail.com>
2022-11-01 15:22:01 +0600

    made the header adaptive

--- js/main.js
+++ js/main.js
@@ -7,10 +7,26 @@
             header.classList.add('header_active')
             active = true;
             console.log(true)
-        } else if (window.pageYOffset <= 50 && active){
+        } else if (window.pageYOffset <= 50 && active) {
             header.classList.remove('header_active')
             active = false;
             console.log(false)
         }
     }
 }())
+
+    // Burger handler
+
+    (function () {
+        const burgerItem = document.querySelector('.burger');
+        const menu = document.querySelector('.header__nav');
+        const menuCloseItem = document.querySelector('.header__nav-close')
+
+        burgerItem.addEventListener('click', () => {
+            menu.classList.add('header__nav_active');
+        })
+
+        menuCloseItem.addEventListener('click', () => {
+            menu.classList.remove('header__nav_active')
+        })
+    }())
