47df81d721532c4a42c5412d04b5cf19504d66cb
ermekm <ermek.mrzv@gmail.com>
2022-11-02 12:13:48 +0600

    added scroll to sections

--- js/main.js
+++ js/main.js
@@ -1,32 +1,72 @@
 (function () {
-    const header = document.querySelector('.header');
-    let active = false;
+	const header = document.querySelector('.header');
+	let active = false;
 
-    window.onscroll = () => {
-        if (window.pageYOffset > 50 && !active) {
-            header.classList.add('header_active')
-            active = true;
-            console.log(true)
-        } else if (window.pageYOffset <= 50 && active) {
-            header.classList.remove('header_active')
-            active = false;
-            console.log(false)
-        }
-    }
-}())
+	window.onscroll = () => {
+		if (window.pageYOffset > 50 && !active) {
+			header.classList.add('header_active')
+			active = true;
+		} else if (window.pageYOffset <= 50 && active) {
+			header.classList.remove('header_active')
+			active = false;
+		}
+	}
+}());
 
-    // Burger handler
+// Burger handler
 
-    (function () {
-        const burgerItem = document.querySelector('.burger');
-        const menu = document.querySelector('.header__nav');
-        const menuCloseItem = document.querySelector('.header__nav-close')
+(function () {
+	const burgerItem = document.querySelector('.burger');
+	const menu = document.querySelector('.header__nav');
+	const menuCloseItem = document.querySelector('.header__nav-close');
 
-        burgerItem.addEventListener('click', () => {
-            menu.classList.add('header__nav_active');
-        })
+	burgerItem.addEventListener('click', () => {
+		menu.classList.add('header__nav_active');
+	})
 
-        menuCloseItem.addEventListener('click', () => {
-            menu.classList.remove('header__nav_active')
-        })
-    }())
+	menuCloseItem.addEventListener('click', () => {
+		menu.classList.remove('header__nav_active');
+	})
+
+}());
+
+
+// Scroll to anchors
+(function () {
+
+	const smoothScroll = function (targetEl, duration) {
+		const headerElHeight = document.querySelector('.header').clientHeight;
+		let target = document.querySelector(targetEl);
+		let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+		let startPosition = window.pageYOffset;
+		let startTime = null;
+
+		const ease = function (t, b, c, d) {
+			t /= d / 2;
+			if (t < 1) return c / 2 * t * t + b;
+			t--;
+			return -c / 2 * (t * (t - 2) - 1) + b;
+		};
+
+		const animation = function (currentTime) {
+			if (startTime === null) startTime = currentTime;
+			const timeElapsed = currentTime - startTime;
+			const run = ease(timeElapsed, startPosition, targetPosition, duration);
+			window.scrollTo(0, run);
+			if (timeElapsed < duration) requestAnimationFrame(animation);
+		};
+		requestAnimationFrame(animation);
+
+	};
+
+	const scrollTo = function () {
+		const links = document.querySelectorAll('.js-scroll');
+		links.forEach(each => {
+			each.addEventListener('click', function () {
+				const currentTarget = this.getAttribute('href');
+				smoothScroll(currentTarget, 1000);
+			});
+		});
+	};
+	scrollTo();
+}());
